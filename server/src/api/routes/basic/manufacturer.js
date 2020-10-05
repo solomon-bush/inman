7637f494accccd3fbcddc0b418b298aee020c7f4
Solomon Bush <m.solomon.bush@gmail.com>
2020-10-05 03:26:30 -0400

    All routes finished for Manufacturer except STATS

--- server/src/api/routes/basic/manufacturer.js
+++ server/src/api/routes/basic/manufacturer.js
@@ -3,22 +3,42 @@ const Manufacturer = require('../../../models/Manufacturer')
 module.exports.set = (app) => {
 
     let url = '/api/manufacturer'
-    // TODO
     // Get All
     app.get(`${url}`, (req, res) => {
-        Manufacturer.findById().then((results) => {
+        Manufacturer.find().then((results) => {
             res.send(results)
         })
     })
 
 
     // Get Details
+    app.get(`${url}/:_id`, (req, res) => {
+        Manufacturer.findById(req.params._id).then(results => { res.send(results) })
+    })
 
-    // Get Stats
-
+    // TODO Get Stats
+    app.get(`${url}/:_id/stats`, (req, res) => {
+        res.send('In Progress')
+    })
 
     // Create New 
-
+    app.post(`${url}`, (req, res) => {
+        let m = new Manufacturer({ ...req.body })
+        m.save().then((result => { res.send(result) })).catch(err => { res.status(404).send(err) })
+    })
     // Update Existing 
+    app.put(`${url}/:_id`, (req, res) => {
+        Manufacturer.findById(req.params._id).then(m => {
+            if (m === null) {
 
+            } else {
+                Manufacturer.findByIdAndUpdate({ _id: req.params._id }, { ...req.body }, { new: true })
+                    .then(result => {
+                        res.send(result)
+                    }).catch(err => {
+                        res.status(404).send(err)
+                    })
+            }
+        })
+    })
 }
